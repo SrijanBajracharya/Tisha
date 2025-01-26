0d2542d32381256de1042cec55ebd7ed48e6a0f6
Srijan Bajracharya <srijanbajracharya@Srijans-MBP.fritz.box>
2025-01-26 20:18:32 +0100

    added event listener

--- src/main/java/com/gemsansar/tisha/user/listener/UserCreatedEventListener.java
+++ src/main/java/com/gemsansar/tisha/user/listener/UserCreatedEventListener.java
@@ -2,6 +2,7 @@ package com.gemsansar.tisha.user.listener;
 
 import com.gemsansar.tisha.user.event.UserCreatedEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +11,7 @@ public class UserCreatedEventListener {
 
     private final UserCreatedEventGateway userCreatedEventGateway;
 
+    @EventListener
     public void handle(UserCreatedEvent event){
         userCreatedEventGateway.create(event.userId());
     }
